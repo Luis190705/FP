da52a859f1d3b1e69f3273e321b7fef5b743a5dd
Luis190705 <luis.sanchezMO35@my.unitec.edu.mx>
2024-10-21 12:08:28 -0600

    Update practica 7.cpp

--- introduccion/practica 7.cpp
+++ introduccion/practica 7.cpp
@@ -70,7 +70,45 @@ int main()
 	}
 }
 //5 Preguntar si al usuario si está a favor de la paz, guardar en una variable tipo char llamada voto, validar si el voto es ‘S’ o el voto es ‘N’ escribir en pantalla “voto valido” en caso de que sea cualquier otro carácter escribir, “voto invalido”
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char voto = ' ';
+    cout << "A favor s/n : ";
+    cin >> voto;
+    cout << endl;
+    if (voto == 's'
+        or voto == 'n' or
+        voto == 'S' or
+        voto == 'N')
+    {
+        cout << "Voto valido "
+            << endl;
+    }
+    else
+    {
+        cout << "Voto invalido "
+            << endl;
+}
 //6 Preguntar al usuario cuantas horas ha trabajado el empleado durante la semana, si, el empleado trabajo más de 40 horas, el excedente de horas se multiplicará por 75 pesos y publicar este cálculo en pantalla que será el bono del empleado.
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int horastrabajadas = 0;
+	cout << "horas trabajadas: ";
+	cin >> horastrabajadas;
+	cout << endl;
+	int horasbono = horastrabajadas - 40;
+	if (horasbono > 0)
+	{
+		horasbono = horasbono * 75;
+		cout << "Bono " << horasbono;
+	}
+}
 //7 Crear dos variables de tipo int, que guarden el día y mes, del cumpleaños del usuario. Si la fecha corresponde a mes 9 y día 15, escribir en pantalla, “Feliz cumpleaños”
 //8 Crear una variable que se llame velocidad, pregunta por este valor, si esta es negativa, escribir en pantalla “velocidad negativa”, si la velocidad es cero, escribir en pantalla “en reposo”, y si es positiva escribir en pantalla “velocidad positiva”.
 //9 Crear las siguientes variables de tipo int: huevos, mantequilla, leche, harina, levadura, azucar y sal. Asigna valores aleatorios a cada variable, entre 1 y 5. Si todos los ingredientes tienen al menos un elemento, escribir en pantalla “Puedes crear hot cakes” de lo contrario escribir en pantalla “No hay suficientes ingredientes”
