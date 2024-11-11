12ad2885b4d64e3ea316dcf5a550622981b03312
Luis190705 <luis.sanchezMO35@my.unitec.edu.mx>
2024-11-11 12:39:22 -0600

    Create 11-11-24 .cpp

---
+++ introduccion/11-11-24 .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>  
+
+using namespace std;
+
+int main() {
+	const int tamaño = 10;
+	int naturales[tamaño]
+	{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	/*cout << naturales[0] << endl;
+	naturales [0] = -1000;
+	cout << naturales[0] << endl;*/
+
+	for (int i = 0; i <= tamaño; i++)
+	{
+		cout << naturales[i] << endl;
+	}
+
+
+}
